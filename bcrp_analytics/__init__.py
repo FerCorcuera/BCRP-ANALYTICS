f0d5d9b9a4ddcb0a826fc05ab0cad433146862a0
Fernando Corcuera <188613791+FerCorcuera@users.noreply.github.com>
2026-05-14 12:25:01 -0500

    Adding first documentation to the realease

--- bcrp_analytics/__init__.py
+++ bcrp_analytics/__init__.py
@@ -1,1 +1,3 @@
 from .utils import build_bcrp_dataset, get_bcrp_clean_series
+
+__version__ = "0.1.0"
--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import find_packages, setup
 
 setup(
     name="bcrp_analytics",
-    version="0.1",
+    version="0.1.0",
+    description="Utilities for retrieving and analyzing BCRP data",
+    author="Fernando Corcuera",
     packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "requests",
+    ],
 )
